5ad6d4010676ed0e34a9c9fe200929550bf7d36d
KillianVrmr <killianvermeer34@gmail.com>
2025-01-13 12:03:34 +0100

    added all the api requests for locations aswell, this is mostly copied over from user with some changes to make it work appropriately with location.

--- laraapi/routes/api.php
+++ laraapi/routes/api.php
@@ -10,14 +10,13 @@ Route::get('/user', function () {
 });
 
 
-
 // Get a user by ID
 Route::get('/user/{id}', function ($id) {
-    $course = DB::select('SELECT * FROM user WHERE id = ?', [$id]);
-    if (empty($course)) {
+    $user = DB::select('SELECT * FROM user WHERE id = ?', [$id]);
+    if (empty($user)) {
         return response()->json(['message' => 'user not found'], 404);
     }
-    return response()->json($course[0]);
+    return response()->json($user[0]);
 });
 
 // Create a new user
@@ -79,4 +78,78 @@ Route::delete('/user/{id}', function ($id) {
         return response()->json(['message' => 'user not found'], 404);
     }
     return response()->json(['message' => 'user deleted successfully']);
+});
+
+// ---------
+// this is where the requests for location
+// ---------
+
+Route::get('/location', function () {
+    $location = DB::select('SELECT * FROM location');
+    return response()->json($location);
+});
+
+// Get a location by ID
+Route::get('/location/{id}', function ($id) {
+    $location = DB::select('SELECT * FROM location WHERE id = ?', [$id]);
+    if (empty($location)) {
+        return response()->json(['message' => 'location not found'], 404);
+    }
+    return response()->json($location[0]);
+});
+
+
+// Create a new location
+Route::post('/location', function (\Illuminate\Http\Request $request) {
+    $name = $request->input('name');
+    $city_id = $request->input('city_id');
+
+    DB::insert('INSERT INTO location (name, city_id) VALUES (?, ?)', [$name, $city_id]);
+
+    return response()->json(['message' => 'location created successfully'], 201);
+});
+
+// Update a location by ID
+Route::put('/location/{id}', function (\Illuminate\Http\Request $request, $id) {
+    $name = $request->input('name');
+    $city_id = $request->input('city_id');
+
+    $affected = DB::update('UPDATE location SET name = ?, city_id = ? WHERE id = ?', [$name, $city_id]);
+
+    if ($affected === 0) {
+        return response()->json(['message' => 'location not found or no changes made'], 404);
+    }
+    return response()->json(['message' => 'location updated successfully']);
+});
+
+Route::patch('/location/{id}', function (\Illuminate\Http\Request $request, $id) {
+    //AI was used for this request
+    $fields = $request->only(['name', 'city_id']); // Get only provided fields
+    if (empty($fields)) {
+        return response()->json(['message' => 'No data provided for update'], 400); // No fields to update
+    }
+
+    $setClause = [];
+    $bindings = [];
+    foreach ($fields as $key => $value) {
+        $setClause[] = "$key = ?";
+        $bindings[] = $value;
+    }
+    $bindings[] = $id; // Add the ID to the bindings
+
+    $query = 'UPDATE location SET ' . implode(', ', $setClause) . ' WHERE id = ?';
+    $affected = DB::update($query, $bindings);
+    if ($affected === 0) {
+        return response()->json(['message' => 'location not found or no changes made'], 404);
+    }
+    return response()->json(['message' => 'location updated successfully']);
+});
+
+// Delete a location by ID
+Route::delete('/location/{id}', function ($id) {
+    $deleted = DB::delete('DELETE FROM location WHERE id = ?', [$id]);
+    if ($deleted === 0) {
+        return response()->json(['message' => 'location not found'], 404);
+    }
+    return response()->json(['message' => 'location deleted successfully']);
 });
